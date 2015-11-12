4e61d6d328c330b4bc9724b456941cc6ee19054d
hepplerj <jason.heppler@gmail.com>
2015-11-12 15:37:51 -0800

    Design tweaks. Beginning work on text selector.

--- palladio-path-component.js
+++ palladio-path-component.js
@@ -3,15 +3,24 @@
 // Most code based on http://bl.ocks.org/hepplerj/e5d3d5787f348cc3b032
 
 angular.module('palladioPathView', ['palladio', 'palladio.services'])
+	.run(['componentService', function(componentService) {
+		var compileStringFunction = function (newScope, options) {
+
+			var compileString = '<div data-palladio-path-view ></div>';
+
+			return compileString;
+		};
+
+		componentService.register('character-path', compileStringFunction);
+	}])
 	.directive('palladioPathView', function (palladioService, dataService) {
 		return {
 			scope : {
 
 			},
 			template : '<div id="main">' +
-						'<h3 style="font: sans-serif;">Chinese Input &#8212; IN DEVELOPMENT</h3>' +
 						'<div id="chart"></div>' +
-					'</div>',
+						'</div>',
 			link : {
 				pre : function(scope, element) {
 					scope.data = {
@@ -24,74 +33,13 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 				post : function(scope, element, attrs) {
 					// Anything that touches the DOM happens here.
 
-					console.log(palladioService);
-					console.log(dataService);
+					var fullData, filteredData;
 
 					var xfilter = dataService.getDataSync().xfilter;
-					var fullData = dataService.getDataSync().data;
+
+					if(xfilter === undefined) { return; }
+
 					var dummyDim = xfilter.dimension(function(d) { return true; });
-
-					var filteredData = dummyDim.top(Infinity);
-
-					var m = d3.map();
-					
-					fullData.forEach(function(d) {
-						// Turn uniq into a number
-						d.uniq = +d.uniq;
-					})
-					
-					// First split up the data by trial/session in a map.
-					fullData.forEach(function(d) {
-						if(m.has([d.trial, d.session])) {
-							m.get([d.trial, d.session]).push(d);
-						} else {
-							m.set([d.trial, d.session], [d]);
-						}
-					});
-					
-					// Sort by uniq, convert to character/token combinations, add to graph
-					var graph = new Graph();
-					m.keys().forEach(function(d) {
-						graph.add(m.get(d)
-							.sort(function(a,b) { return a.uniq - b.uniq; })
-							.map(function(d) { return d.chinese + "," + d.token; }));
-					});
-					
-					// Generate the node ordering using topological sort.
-					// !! This won't work if there are any cycles or miscategorized selection keystrokes.
-					// Array of node strings for index lookup purposes
-					var stringNodeArray = graph.sort();
-					// Array of objects for rendering.
-					var nodeArray = graph.sort()
-						.map(function(d, i) { 
-							return { 
-								chinese: d.split(",")[0], 
-								token: d.split(",")[1],
-								uniq: i
-							}; 
-						});
-
-					scope.data.nodes = nodeArray;
-					
-					// Now go through all the trials and create our links.
-					var linkArray = [];
-					m.keys().forEach(function(d) {
-						// Current trial
-						var current = m.get(d);
-						// Iterate through each keystroke in the current trial
-						for(var i=0; i < current.length - 1; i++) {
-							// We are not at the end (there is no link at the end)
-							linkArray.push({
-								source: stringNodeArray.indexOf(current[i].chinese + "," + current[i].token),
-								target: stringNodeArray.indexOf(current[i+1].chinese + "," + current[i+1].token),
-								session: +current[i].session,
-								trial: +current[i].trial,
-								segment: 1
-							});
-						};
-					});
-					
-					scope.data.links = linkArray;
 
 					var width   = 960,
 					    height  = 200,
@@ -118,6 +66,22 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					  .classed("tooltip", true)
 					  .classed("hidden", true);
 
+					var text_opts = {
+					  "tangPoem": {
+					    "field": "tangPoem",
+					    "label": "Tang poem"
+					  },
+					  "poem2": {
+					    "field": "poem2",
+					    "label": "Poem 2"
+					  }
+					};
+
+					initialize();
+					update();
+
+					palladioService.onUpdate("path-component", update);
+
 					// Main
 					//-----------------------------------------------------
 					function arcDiagram(graph) {
@@ -125,10 +89,15 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					    .domain([0, 20])
 					    .range([0, 15]);
 
+			      // Remove SVG if it already exists (this is a very inefficient way to handle updates...)
+					  if(!d3.select(element[0]).select("svg").empty()) {
+					    d3.select(element[0]).select("svg").remove();
+					  }
+
 					  var svg = d3.select(element[0]).append("svg")
-					      .attr("id", "arc")
-					      .attr("width", width)
-					      .attr("height", height);
+						      .attr("id", "arc")
+						      .attr("width", width)
+						      .attr("height", height);
 
 					  // create plot within svg
 					  var wrapper = svg.append("g")
@@ -182,7 +151,7 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 
 					function drawNodes(nodes) {
 
-					  var gnodes = d3.select("#wrapper").selectAll("g.node")
+					  var gnodes = d3.select(element[0]).select('svg').select('#wrapper').selectAll("g.node")
 					    .data(nodes);
 
 					  var nodeEnter = gnodes.enter()
@@ -197,7 +166,7 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					    .attr("r", 14)
 					    .attr("stroke-width","2.5px")
 					    .attr("fill","#ffffff")
-					    .style("stroke", function(d, i) { return color(d.type); });
+					    .style("stroke", function(d, i) { return color(d.tokenType); });
 
 					  // Handling mouseover functions
 					  // nodeEnter.selectAll(".node")
@@ -215,11 +184,13 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					    .style("text-anchor", "middle")
 					    .attr("dx", function(d) { return d.x; })
 					    .attr("dy", function(d) { return d.y + 5; })
-					    .text(function(d) { return d.token; })
-					    .style("font","10px sans-serif");
+					    .text(function(d) {
+					      // hide text in selection nodes
+					      if (d.tokenType === "selection") { return null; }
+					      else { return d.token; }
+					    })
+					    .style("font","12px sans-serif");
 
-					  // d3.select("#trial2")
-					  //   .on("mouseover", trialOver);
 					}
 
 					function drawLinks(links) {
@@ -231,7 +202,7 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					    .tension(0)
 					    .angle(function(d) { return radians(d); });
 
-					  d3.select("#wrapper").selectAll(".link")
+					  d3.select(element[0]).select('svg').select('#wrapper').selectAll(".link")
 					    .data(links)
 					  .enter().append("path")
 					    .attr("class", "link")
@@ -282,40 +253,145 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					   "</table>";
 					}
 
-					function segmentHighlight(streamHighlight) {
-					  if(d3.selectAll("path").filter(streamHighlight).classed("highlighted") === true){
-					    d3.selectAll("path").filter(streamHighlight).classed("highlighted", false);
-					  } else {
-					    d3.selectAll("path").filter(streamHighlight).classed("highlighted", true);
-					  }
+					// Field selector
+					function create_field_selector() {
+						var fieldSelector = d3.select("#field-selector")
+						      // .on("change", fieldSelected);
+
+						for (var key in text_opts) {
+						  fieldSelector.append("option")
+						  .attr("value", key)
+						  .text(text_opts[key].label);
+						}
 					}
 
-					// function resetHighlight() {
-					//   d3.selectAll("path").style("display","block");
-					// }
-
-					function nodeOver(d,i) {
-					  d3.selectAll("path").style("stroke", function (p) {return p.source == d || p.target == d ? "#17becf" : "#888888"})
+					function shallowCopy(obj) {
+						var copy = {};
+						for (var attr in obj) {
+							if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+						}
+						return copy;
 					}
 
-					function edgeOver(d) {
-					  d3.selectAll("path").style("stroke", function(p) {return p == d ? "#17becf" : "#888888"})
+					// Objects must have same properties
+					// ignores the 'uniq' property, which is special in our case, and 'generate' property.
+					function shallowCompare(obj1, obj2) {
+						for (var attr in obj1) {
+							if(obj1[attr] !== obj2[attr] && attr !== 'uniq'  && attr !== 'generated') {
+								if(attr === 'uniq') console.log(attr + ":" + obj1[attr] + ":" + obj2[attr]);
+								return false;
+							}
+						}
+						return true;
 					}
 
-					function transition(path) {
-					  path.transition()
-					    .duration(2500)
-					    .attrTween("stroke-dasharray", tweenDash)
-					    .each("end", function() { d3.select(this).call(transition); });
+					function shallowContains(arr, obj) {
+						return arr.filter(shallowCompare.bind(null, obj)).length > 0 ? true : false;
 					}
 
-					function tweenDash() {
-					  var l = this.getTotalLength(),
-					      i = d3.interpolateString("0," + l, l + "," + l);
-					    return function(t) { return i(t); };
+					function buildNodesAndLinks() {
+
+						fullData = dataService.getDataSync().data;
+						filteredData = dummyDim.top(Infinity);
+
+						var m = d3.map();
+						var mFiltered = d3.map();
+
+						fullData.forEach(function(d) {
+							// Turn uniq into a number
+							d.uniq = +d.uniq;
+						});
+
+						filteredData.forEach(function(d) {
+							// Turn uniq into a number
+							d.uniq = +d.uniq;
+						});
+
+						// First split up the data by trial/session in a map.
+						var copy = {};
+						fullData.forEach(function(d) {
+							copy = shallowCopy(d);
+							copy.occur = 0;
+							if(m.has([d.trial, d.session])) {
+								while(shallowContains(m.get([d.trial, d.session]), copy)) {
+									copy.occur++;
+								}
+								m.get([d.trial, d.session]).push(copy);
+							} else {
+								m.set([d.trial, d.session], [copy]);
+							}
+						});
+
+						filteredData.forEach(function(d) {
+							if(mFiltered.has([d.trial, d.session])) {
+								mFiltered.get([d.trial, d.session]).push(d);
+							} else {
+								mFiltered.set([d.trial, d.session], [d]);
+							}
+						});
+
+						// Sort by uniq, convert to character/token combinations, add to graph
+						var graph = new Graph();
+						m.keys().forEach(function(d) {
+							var sequence = [];
+							var i = 0;
+
+							m.get(d)
+								.sort(function(a,b) { return a.uniq - b.uniq; })
+								.forEach(function(d) {
+									sequence.push(d.chinese + "," + d.token + "," + d.token_type + "," + d.occur);
+								});
+
+							graph.add(sequence);
+						});
+
+						// Generate the node ordering using topological sort.
+						// !! This won't work if there are any cycles or miscategorized selection keystrokes.
+						// Array of node strings for index lookup purposes
+						var stringNodeArray = graph.sort();
+						// Array of objects for rendering.
+						var nodeArray = graph.sort()
+							.map(function(d, i) {
+								return {
+									chinese: d.split(",")[0],
+									token: d.split(",")[1],
+									tokenType: d.split(",")[2],
+									uniq: i
+								};
+							});
+
+						scope.data.nodes = nodeArray;
+
+						// Now go through all the trials and create our links.
+						var linkArray = [];
+						mFiltered.keys().forEach(function(d) {
+							// Current trial
+							var current = m.get(d);
+							// Iterate through each keystroke in the current trial
+							for(var i=0; i < current.length - 1; i++) {
+								// We are not at the end (there is no link at the end)
+								linkArray.push({
+									source: stringNodeArray.indexOf(current[i].chinese + "," + current[i].token + "," + current[i].token_type + "," + current[i].occur),
+									target: stringNodeArray.indexOf(current[i+1].chinese + "," + current[i+1].token + "," + current[i+1].token_type + "," + current[i+1].occur),
+									session: +current[i].session,
+									trial: +current[i].trial,
+									segment: 1
+								});
+							}
+						});
+
+						scope.data.links = linkArray;
 					}
 
-					arcDiagram(scope.data);
+					function initialize() {
+
+					}
+
+					function update() {
+						buildNodesAndLinks();
+						arcDiagram(scope.data);
+						create_field_selector();
+					}
 				}
 			}
 		};
